6b194e5015279d46ad839f392fa1e3212bed158a
Narasimharaju1239 <narasimharaju1239@gmail.com>
2025-08-11 22:18:48 +0530

    Add production Netlify domain to CORS allowed origins

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://6899ee6ae42fb400080d45aa--srisanthoshimathaaquabazar.netlify.app'
+    'https://6899ee6ae42fb400080d45aa--srisanthoshimathaaquabazar.netlify.app',
+    'https://srisanthoshimathaaquabazar.netlify.app'
   ],
   credentials: true
 }));
